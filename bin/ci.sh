b4f90c63413258680c2544c85c4ff7332abe9c04
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2017-02-04 08:24:14 +0100

    Do not htmlproof disabled links - aka #

--- bin/ci.sh
+++ bin/ci.sh
@@ -4,4 +4,4 @@ set -e # halt script on error
 bundle exec jekyll build
 
 LAST_POST=$(ls _posts/ -1 | sort | tail -n 1 | sed 's/[0-9]*-[0-9]*-[0-9]*-//g' | sed 's/.md$//g')
-bundle exec htmlproofer --url-ignore "http://prestodb.rocks/${LAST_POST}/" ./_site
+bundle exec htmlproofer --url-ignore "http://prestodb.rocks/${LAST_POST}/,#" ./_site
